5d5ec7157e0decce9e8d7b0cde77beb09dd41eef
Petr Mensik <petr.mensik1@gmail.com>
2020-10-27 16:29:15 +0100

    Add Contract pallete to the chain

--- node/src/rpc.rs
+++ node/src/rpc.rs
@@ -7,12 +7,13 @@
 
 use std::sync::Arc;
 
-use node_template_runtime::{opaque::Block, AccountId, Balance, Index};
 use sp_api::ProvideRuntimeApi;
 use sp_blockchain::{Error as BlockChainError, HeaderMetadata, HeaderBackend};
 use sp_block_builder::BlockBuilder;
 pub use sc_rpc_api::DenyUnsafe;
 use sp_transaction_pool::TransactionPool;
+use node_template_runtime::{opaque::Block, AccountId, Balance, Index, BlockNumber};
+use pallet_contracts_rpc::{Contracts, ContractsApi};
 
 
 /// Full client dependencies.
@@ -34,6 +35,7 @@ pub fn create_full<C, P>(
 	C: Send + Sync + 'static,
 	C::Api: substrate_frame_rpc_system::AccountNonceApi<Block, AccountId, Index>,
 	C::Api: pallet_transaction_payment_rpc::TransactionPaymentRuntimeApi<Block, Balance>,
+    C::Api: pallet_contracts_rpc::ContractsRuntimeApi<Block, AccountId, Balance, BlockNumber>,
 	C::Api: BlockBuilder<Block>,
 	P: TransactionPool + 'static,
 {
@@ -54,6 +56,9 @@ pub fn create_full<C, P>(
 	io.extend_with(
 		TransactionPaymentApi::to_delegate(TransactionPayment::new(client.clone()))
 	);
+    io.extend_with(
+        ContractsApi::to_delegate(Contracts::new(client.clone()))
+    );
 
 	// Extend this RPC with a custom API by using the following syntax.
 	// `YourRpcStruct` should have a reference to a client, which is needed
